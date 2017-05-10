d8ba017c9e139938f586a37091a9d3dc78908694
kenneth <kmbuthia47@gmail.com>
2017-05-11 00:07:00 +0100

    Added dummy charts to both pages

---
+++ dist/js/dummycharts.js
@@ -0,0 +1,412 @@
+//Horizontal bar chart
+Highcharts.chart('bar_horizontal', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: ''
+    },
+    xAxis: {
+        type: 'category'
+    },
+    yAxis: {
+        title: {
+            text: 'Total percent market share'
+        }
+
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}%'
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Company A',
+            y: 56.33,
+            drilldown: 'Company A'
+        }, {
+            name: 'Company B',
+            y: 24.03,
+            drilldown: 'Company B'
+        }, {
+            name: 'Company C',
+            y: 10.38,
+            drilldown: 'Company C'
+        }, {
+            name: 'Company D',
+            y: 4.77,
+            drilldown: 'Company D'
+        }, {
+            name: 'Company E',
+            y: 0.91,
+            drilldown: 'Company E'
+        }]
+    }],
+    drilldown: {
+        series: [{
+            name: 'Microsoft Internet Explorer',
+            id: 'Microsoft Internet Explorer',
+            data: [
+                [
+                    'v11.0',
+                    24.13
+                ],
+                [
+                    'v8.0',
+                    17.2
+                ],
+                [
+                    'v9.0',
+                    8.11
+                ],
+                [
+                    'v10.0',
+                    5.33
+                ],
+                [
+                    'v6.0',
+                    1.06
+                ],
+                [
+                    'v7.0',
+                    0.5
+                ]
+            ]
+        }, {
+            name: 'Chrome',
+            id: 'Chrome',
+            data: [
+                [
+                    'v40.0',
+                    5
+                ],
+                [
+                    'v41.0',
+                    4.32
+                ],
+                [
+                    'v42.0',
+                    3.68
+                ],
+                [
+                    'v39.0',
+                    2.96
+                ],
+                [
+                    'v36.0',
+                    2.53
+                ],
+                [
+                    'v43.0',
+                    1.45
+                ],
+                [
+                    'v31.0',
+                    1.24
+                ],
+                [
+                    'v35.0',
+                    0.85
+                ],
+                [
+                    'v38.0',
+                    0.6
+                ],
+                [
+                    'v32.0',
+                    0.55
+                ],
+                [
+                    'v37.0',
+                    0.38
+                ],
+                [
+                    'v33.0',
+                    0.19
+                ],
+                [
+                    'v34.0',
+                    0.14
+                ],
+                [
+                    'v30.0',
+                    0.14
+                ]
+            ]
+        }, {
+            name: 'Firefox',
+            id: 'Firefox',
+            data: [
+                [
+                    'v35',
+                    2.76
+                ],
+                [
+                    'v36',
+                    2.32
+                ],
+                [
+                    'v37',
+                    2.31
+                ],
+                [
+                    'v34',
+                    1.27
+                ],
+                [
+                    'v38',
+                    1.02
+                ],
+                [
+                    'v31',
+                    0.33
+                ],
+                [
+                    'v33',
+                    0.22
+                ],
+                [
+                    'v32',
+                    0.15
+                ]
+            ]
+        }, {
+            name: 'Safari',
+            id: 'Safari',
+            data: [
+                [
+                    'v8.0',
+                    2.56
+                ],
+                [
+                    'v7.1',
+                    0.77
+                ],
+                [
+                    'v5.1',
+                    0.42
+                ],
+                [
+                    'v5.0',
+                    0.3
+                ],
+                [
+                    'v6.1',
+                    0.29
+                ],
+                [
+                    'v7.0',
+                    0.26
+                ],
+                [
+                    'v6.2',
+                    0.17
+                ]
+            ]
+        }, {
+            name: 'Opera',
+            id: 'Opera',
+            data: [
+                [
+                    'v12.x',
+                    0.34
+                ],
+                [
+                    'v28',
+                    0.24
+                ],
+                [
+                    'v27',
+                    0.17
+                ],
+                [
+                    'v29',
+                    0.16
+                ]
+            ]
+        }]
+    }
+});
+
+//Vertical bar chart
+Highcharts.chart('bar_vertical', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: ''
+    },
+    xAxis: {
+        categories: ['Money', 'Transport', 'Lunch'],
+        title: {
+            text: null
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Population (millions)',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    tooltip: {
+        valueSuffix: ' millions'
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+        shadow: true
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Money',
+        data: [107, 31, 635]
+    }, {
+        name: 'Transport',
+        data: [133, 156, 947]
+    }, {
+        name: 'Lunch',
+        data: [1052, 954, 4250]
+    }]
+});
+
+//Grouped bar chart
+Highcharts.chart('bar_grouped', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: ''
+    },
+    xAxis: {
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Rainfall (mm)'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Prescription amount',
+        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+    }, {
+        name: 'Payment amount',
+        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+    }]
+});
+
+//Pie chart
+/*Highcharts.chart('pie_chart', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares January, 2015 to May, 2015'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Microsoft Internet Explorer',
+            y: 56.33
+        }, {
+            name: 'Chrome',
+            y: 24.03,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Firefox',
+            y: 10.38
+        }, {
+            name: 'Safari',
+            y: 4.77
+        }, {
+            name: 'Opera',
+            y: 0.91
+        }, {
+            name: 'Proprietary or Undetectable',
+            y: 0.2
+        }]
+    }]
+});*/
---
+++ dist/js/dummychartstwo.js
@@ -0,0 +1,316 @@
+Highcharts.chart('line_graph', {
+
+    title: {
+        text: ''
+    },
+
+    yAxis: {
+        title: {
+            text: 'Cost ($)'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            pointStart: 2010
+        }
+    },
+
+    series: [{
+        name: 'Claims',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }, {
+        name: 'Purchases',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    }]
+
+});
+
+//Vertical bar chart
+Highcharts.chart('bar_vertical', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: ''
+    },
+    xAxis: {
+        type: 'category'
+    },
+    yAxis: {
+        title: {
+            text: 'Total percent market share'
+        }
+
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}%'
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Doctor A',
+            y: 56.33,
+            drilldown: 'Doctor A'
+        }, {
+            name: 'Doctor B',
+            y: 24.03,
+            drilldown: 'Doctor B'
+        }, {
+            name: 'Doctor C',
+            y: 10.38,
+            drilldown: 'Doctor C'
+        }, {
+            name: 'Doctor D',
+            y: 4.77,
+            drilldown: 'Doctor D'
+        }, {
+            name: 'Doctor E',
+            y: 0.91,
+            drilldown: 'Doctor E'
+        }]
+    }],
+    drilldown: {
+        series: [{
+            name: 'Microsoft Internet Explorer',
+            id: 'Microsoft Internet Explorer',
+            data: [
+                [
+                    'v11.0',
+                    24.13
+                ],
+                [
+                    'v8.0',
+                    17.2
+                ],
+                [
+                    'v9.0',
+                    8.11
+                ],
+                [
+                    'v10.0',
+                    5.33
+                ],
+                [
+                    'v6.0',
+                    1.06
+                ],
+                [
+                    'v7.0',
+                    0.5
+                ]
+            ]
+        }, {
+            name: 'Chrome',
+            id: 'Chrome',
+            data: [
+                [
+                    'v40.0',
+                    5
+                ],
+                [
+                    'v41.0',
+                    4.32
+                ],
+                [
+                    'v42.0',
+                    3.68
+                ],
+                [
+                    'v39.0',
+                    2.96
+                ],
+                [
+                    'v36.0',
+                    2.53
+                ],
+                [
+                    'v43.0',
+                    1.45
+                ],
+                [
+                    'v31.0',
+                    1.24
+                ],
+                [
+                    'v35.0',
+                    0.85
+                ],
+                [
+                    'v38.0',
+                    0.6
+                ],
+                [
+                    'v32.0',
+                    0.55
+                ],
+                [
+                    'v37.0',
+                    0.38
+                ],
+                [
+                    'v33.0',
+                    0.19
+                ],
+                [
+                    'v34.0',
+                    0.14
+                ],
+                [
+                    'v30.0',
+                    0.14
+                ]
+            ]
+        }, {
+            name: 'Firefox',
+            id: 'Firefox',
+            data: [
+                [
+                    'v35',
+                    2.76
+                ],
+                [
+                    'v36',
+                    2.32
+                ],
+                [
+                    'v37',
+                    2.31
+                ],
+                [
+                    'v34',
+                    1.27
+                ],
+                [
+                    'v38',
+                    1.02
+                ],
+                [
+                    'v31',
+                    0.33
+                ],
+                [
+                    'v33',
+                    0.22
+                ],
+                [
+                    'v32',
+                    0.15
+                ]
+            ]
+        }, {
+            name: 'Safari',
+            id: 'Safari',
+            data: [
+                [
+                    'v8.0',
+                    2.56
+                ],
+                [
+                    'v7.1',
+                    0.77
+                ],
+                [
+                    'v5.1',
+                    0.42
+                ],
+                [
+                    'v5.0',
+                    0.3
+                ],
+                [
+                    'v6.1',
+                    0.29
+                ],
+                [
+                    'v7.0',
+                    0.26
+                ],
+                [
+                    'v6.2',
+                    0.17
+                ]
+            ]
+        }, {
+            name: 'Opera',
+            id: 'Opera',
+            data: [
+                [
+                    'v12.x',
+                    0.34
+                ],
+                [
+                    'v28',
+                    0.24
+                ],
+                [
+                    'v27',
+                    0.17
+                ],
+                [
+                    'v29',
+                    0.16
+                ]
+            ]
+        }]
+    }
+});
+
+Highcharts.chart('pie_chart', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: ''
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
+    series: [{
+        name: 'Drug cost',
+        colorByPoint: true,
+        data: [{
+            name: 'Cost of drug',
+            y: 56.33
+        }, {
+            name: 'Average cost of alternatives',
+            y: 24.03,
+            sliced: true,
+            selected: true
+        }]
+    }]
+});
